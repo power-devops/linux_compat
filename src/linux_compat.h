23e438791040a52f1b0782253e1afebb97b9f953
aklyachkin <aklyachkin@gmail.com>
2020-07-17 14:52:15 +0200

    sethostname() declaration

--- src/linux_compat.h
+++ src/linux_compat.h
@@ -6,5 +6,7 @@
 int futimes(int, struct timeval *);
 time_t timegm(struct tm *);
 char *getprogname();
+/* the function is in libc.a, but it is not defined in any header file */
+int sethostname(char *, int);
 
 #endif
